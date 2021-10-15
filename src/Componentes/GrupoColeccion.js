92a3474f6e51ac0701c3848c66cf8937cd8faeea
lchaconc <luis.chacon.campos@gmail.com>
2021-10-15 10:09:39 -0600

    Fase despliegue -Imp de estado colecciones -Setup para creación de colecciones -Filtrado de recursos por seccion (colección)

--- src/Componentes/GrupoColeccion.js
+++ src/Componentes/GrupoColeccion.js
@@ -5,88 +5,45 @@ import Coleccion from "./Coleccion";
 function GrupoColeccion(props) {
   const recursos = props.recursos;
   const secciones = props.secciones;
+  //Arreglo de arreglos con recursos por colección. Se carga en setup pAra luego ser renderizado en componente (return)
+  //let vistasColecciones = [];
+  const [vistasColecciones, setVistasColecciones] = useState([]);
 
   useEffect(() => {
     //console.log(secciones);
+    console.log("iniciando componente grupo");
     setup();
   }, []);
 
   const setup = () => {
     let tmpArray = [];
     secciones.forEach((seccion) => {
-      console.log(seccion.nombre);
+      //console.log(seccion.nombre);
       recursos.forEach((item) => {
         if (item.seccion === seccion.nombre) {
           tmpArray.push(item);
         }
       });
-      console.log(tmpArray);
+      //console.log(tmpArray);
+      //vistasColecciones.push(tmpArray);
+      setVistasColecciones(vistasColecciones.concat(tmpArray));
       tmpArray = [];
     });
+    console.log("setup terminado");
+    //console.log("vistasColecciones", vistasColecciones);
   };
 
   return (
-    <React.Fragment>
-      {props.lista2021 && (
-        <Coleccion
-          tabIndex={5}
-          titulo="Colección 2021"
-          array={props.lista2021}
-        />
-      )}
-      <br />
-      {props.lista2020 && (
-        <Coleccion
-          tabIndex={6}
-          titulo="Colección 2020"
-          array={props.lista2020}
-        />
-      )}
-      <br />
-      {props.lista2019 && (
-        <Coleccion
-          tabIndex={7}
-          titulo="Colección 2019"
-          array={props.lista2019}
-        />
-      )}
-      <br />
-      {props.lista2018 && (
-        <Coleccion
-          tabIndex={8}
-          titulo="Colección 2018"
-          array={props.lista2018}
-        />
-      )}
-      <br />
-      {props.lista2017 && (
-        <Coleccion
-          tabIndex={9}
-          titulo="Colección 2017"
-          array={props.lista2017}
-        />
-      )}
-      <br />
-      {props.listaAnteriores && (
-        <Coleccion
-          tabIndex={10}
-          titulo="Anteriores"
-          array={props.listaAnteriores}
-        />
-      )}
-      <br />
-      {props.listaprofe && (
-        <Coleccion
-          tabIndex={11}
-          titulo="Profe en casa"
-          array={props.listaprofe}
-        />
-      )}
-      <br />
-      {props.listaOtros && (
-        <Coleccion tabIndex={12} titulo="Otros" array={props.listaOtros} />
-      )}
-    </React.Fragment>
+    vistasColecciones &&
+    vistasColecciones.map((item, i) => (
+      <Coleccion
+        tabIndex={i + 1}
+        key={i}
+        //titulo={`Colección ${secciones[i].nombre}`}
+        titulo="prb"
+        array={item}
+      />
+    ))
   );
 }
 
