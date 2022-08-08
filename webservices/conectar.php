ba94817011dd22d289f3e37bc6c41aa90bbf3375
Patricia Hernandez Conejo <phernandezc@intranet.mep.go.cr>
2022-08-08 11:12:13 -0600

    Documentos de referencia actualización

--- webservices/conectar.php
+++ webservices/conectar.php
@@ -16,7 +16,7 @@ function conectarDB(){
         if($conexion){
             echo "";
         }else{
-            echo 'Ha sucedido un error inexperado en la conexion de la base de datos
+            echo 'Ha sucedido un error inesperado en la conexion de la base de datos
 ';
         }
 
